b3519a9719f449092e3c44ccffd62b5c5aac9371
Victor <53414263+vuduland@users.noreply.github.com>
2019-12-04 14:04:36 -0600

    ignored .DS_Store

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,7 @@
 
 A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
 */
 
 var surveyData = require('../data/friends');
@@ -18,7 +19,39 @@ module.exports = function (app) {
   // user submits form -> server -> data to surveyData array
 
   app.post('', (req, res) => {
-    app.get()
+
   });
 
 }
+
+
+//example from hotrestaurant solved
+
+
+
+
+app.post("/api/tables", function (req, res) {
+  // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+  // It will do this by sending out the value "true" have a table
+  // req.body is available since we're using the body parsing middleware
+  if (tableData.length < 5) {
+    tableData.push(req.body);
+    res.json(true);
+  }
+  else {
+    waitListData.push(req.body);
+    res.json(false);
+  }
+});
+
+// ---------------------------------------------------------------------------
+// I added this below code so you could clear out the table while working with the functionality.
+// Don"t worry about it!
+
+app.post("/api/clear", function (req, res) {
+  // Empty out the arrays of data
+  tableData.length = 0;
+  waitListData.length = 0;
+
+  res.json({ ok: true });
+});
